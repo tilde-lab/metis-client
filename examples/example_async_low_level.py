ba7d63a05d9199b94dbf9528af8b68a8d1ede5de
Sergei Korolev <knopki@duck.com>
2023-02-28 15:20:30 +0300

    chore: update docs

--- examples/example_async_low_level.py
+++ examples/example_async_low_level.py
@@ -1,69 +1,55 @@
 #!/usr/bin/env python3
+"""Example of usage of asynchronous client with low-level methods"""
 
-import signal
+import asyncio
 import sys
-
-from anyio import CancelScope, create_task_group, open_signal_receiver, run
-from yarl import URL
+from datetime import datetime
 
 from metis_client import MetisAPIAsync, MetisTokenAuth
-from metis_client.models import MetisCalculationsEventModel, MetisDataSourcesEventModel
 
-API_URL = URL("http://localhost:3000")
+API_URL = "http://localhost:3000"
 
 try:
-    content = open(sys.argv[1]).read()
+    with open(sys.argv[1], encoding="utf-8") as fp:
+        CONTENT = fp.read()
 except IndexError:
-    content = """{"attributes":{"immutable_id":42, "species":[{"chemical_symbols":
+    CONTENT = """{"attributes":{"immutable_id":42, "species":[{"chemical_symbols":
 ["Au"]}],"cartesian_site_positions":[[0,0,0]],"lattice_vectors":[[0,2,2],[2,0,2],[2,2,0]]}}"""
 
 
-async def signal_handler(scope: CancelScope):
-    with open_signal_receiver(signal.SIGINT, signal.SIGTERM) as signals:
-        async for signum in signals:
-            if signum == signal.SIGINT:
-                print("Ctrl+C pressed!")
-            else:
-                print("Terminated!")
-
-            scope.cancel()
-            return
-
-
-async def job():
+async def main():
+    "Do work"
     async with MetisAPIAsync(API_URL, auth=MetisTokenAuth("admin@test.com")) as client:
         print(await client.v0.auth.whoami())
         async with client.stream.subscribe() as sub:
-            req = await client.v0.datasources.create_event(content)
+            req = await client.v0.datasources.create_event(CONTENT)
             answer = None
             async for msg in sub:
-                if (
-                    MetisDataSourcesEventModel.guard(msg)
-                    and msg.request_id == req.request_id
-                ):
-                    answer = msg
+                if msg["type"] == "datasources" and msg.get("data", {}).get(
+                    "reqId"
+                ) == req.get("reqId"):
+                    answer = msg.get("data")
                     break
-            if not answer or not answer.data:
+            if not answer:
                 return None
 
-            data_id = sorted(answer.data, key=lambda x: x.created_at)[-1].id
+            data_id = sorted(
+                answer["data"],
+                key=lambda x: x.get("createdAt", datetime.fromordinal(1)),
+            )[-1].get("id")
+
             req = await client.v0.calculations.create_event(data_id)
+            answer = None
             async for msg in sub:
-                if (
-                    MetisCalculationsEventModel.guard(msg)
-                    and msg.request_id == req.request_id
-                ):
-                    answer = msg
+                if msg["type"] == "calculations" and msg.get("data", {}).get(
+                    "reqId"
+                ) == req.get("reqId"):
+                    data = msg.get("data", {}).get("data", [])
+                    if data:
+                        answer = data[-1]
                     break
             print(answer)
             print("=" * 100 + "Test passed")
 
 
-async def main():
-    async with create_task_group() as tg:
-        tg.start_soon(signal_handler, tg.cancel_scope)
-        await job()
-        tg.cancel_scope.cancel()
-
-
-run(main)
+asyncio.run(main())
